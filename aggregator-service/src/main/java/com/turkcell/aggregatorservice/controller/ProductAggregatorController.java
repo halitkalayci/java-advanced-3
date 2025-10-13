3a43979f4171ad82813050f808571cb6e8e41480
halitklyc <halitenes.kalayci@gmail.com>
2025-10-13 14:44:04 +0300

    aggregator eklendi.

---
+++ aggregator-service/src/main/java/com/turkcell/aggregatorservice/controller/ProductAggregatorController.java
@@ -0,0 +1,47 @@
+package com.turkcell.aggregatorservice.controller;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/product-aggregator")
+public class ProductAggregatorController {
+    private final WebClient.Builder webClientBuilder;
+
+    public ProductAggregatorController(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+    @GetMapping
+    public Mono<AggregatedProductDetail> getAggregatedProductDetail(@RequestParam int productId) {
+        // http://localhost:8888/api/v1/products/1
+        Mono<Product> productMono = webClientBuilder
+                .build()
+                .get()
+                .uri("http://gateway-server/api/v1/products/"+1)
+                .retrieve()
+                .bodyToMono(Product.class);
+
+        Mono<List<Review>> reviewsMono = webClientBuilder
+                .build()
+                .get()
+                .uri("http://gateway-server/api/v2/reviews?productId="+1)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Review>>() {});
+        // zip -> iki isteği PARALEL çalıştır, sonuçları birleştir.
+        return Mono
+                .zip(productMono, reviewsMono)
+                .map(tuple -> new AggregatedProductDetail(tuple.getT1(), tuple.getT2()));
+    }
+
+
+    record Product(int id, String name, String description) {}
+    record Review(int id, String author, int rating, String description) {}
+    record AggregatedProductDetail(Product product, List<Review> reviews) {}
+}
