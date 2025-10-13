e76ec3ac3b98436100e32fd4232b2fc8ebfc3a44
halitklyc <halitenes.kalayci@gmail.com>
2025-10-13 10:54:55 +0300

    add V2 execution & yml

---
+++ review-service/src/main/java/com/turkcell/reviewservice/controller/ReviewsV2Controller.java
@@ -0,0 +1,18 @@
+package com.turkcell.reviewservice.controller;
+
+import com.turkcell.reviewservice.contract.v2.api.ReviewsV2Api;
+import com.turkcell.reviewservice.contract.v2.model.CreateReview201Response;
+import com.turkcell.reviewservice.contract.v2.model.CreateReviewRequest;
+import com.turkcell.reviewservice.contract.v2.model.CreateReviewRequestV2;
+import com.turkcell.reviewservice.contract.v2.model.CreateReviewV2201Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewsV2Controller implements ReviewsV2Api {
+
+    @Override
+    public ResponseEntity<CreateReviewV2201Response> createReviewV2(CreateReviewRequestV2 createReviewRequestV2) {
+        return null;
+    }
+}
