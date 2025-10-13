b8b48dbe0cda6ffb25da2fc92e26c0d7e7db8125
halitklyc <halitenes.kalayci@gmail.com>
2025-10-13 13:23:46 +0300

    review service discovery-server'a eklendi.

--- order-service/src/main/java/com/turkcell/orderservice/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/orderservice/controller/OrdersController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.OffsetDateTime;
+
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
     @GetMapping
     public String getOrders() {
+        System.out.println("Order servis çalıştı. " + OffsetDateTime.now().toString());
         return "Order Service";
     }
 }
