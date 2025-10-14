8d2493a782dad13dc64f34ccff3cb15acc0ed94e
halitklyc <halitenes.kalayci@gmail.com>
2025-10-14 11:29:25 +0300

    kafka event publishing

--- order-service/src/main/java/com/turkcell/orderservice/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/orderservice/controller/OrdersController.java
@@ -4,6 +4,7 @@ import com.turkcell.orderservice.client.ProductClient;
 import com.turkcell.orderservice.client.StaticProductClient;
 import com.turkcell.orderservice.contract.GetProductByIdContract;
 import com.turkcell.orderservice.dto.CreateOrderRequest;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,11 +16,13 @@ public class OrdersController {
     private final RestTemplate restTemplate;
     private final ProductClient productClient;
     private final StaticProductClient staticProductClient;
+    private final StreamBridge streamBridge;
 
-    public OrdersController(RestTemplate restTemplate, ProductClient productClient, StaticProductClient staticProductClient) {
+    public OrdersController(RestTemplate restTemplate, ProductClient productClient, StaticProductClient staticProductClient, StreamBridge streamBridge) {
         this.restTemplate = restTemplate;
         this.productClient = productClient;
         this.staticProductClient = staticProductClient;
+        this.streamBridge = streamBridge;
     }
 
     @GetMapping
@@ -34,18 +37,11 @@ public class OrdersController {
         // Sync iletişim.
         for(CreateOrderRequest.OrderProductItem item: order.getItems())
         {
-            /*String url = "http://product-service/api/v1/products/"+item.productId();
-            var response = restTemplate.getForEntity(url, GetProductByIdContract.class)
-                    .getBody();
-            System.out.println(response.name() + " ürünü için stok okundu: " + response.stock());
-            if(response.stock() < item.quantity())
-                throw new RuntimeException(response.name() +  " ürünü için stok değeri yetersiz.");*/
-
             GetProductByIdContract response = productClient.getProductById(item.productId());
             if(response.stock() < item.quantity())
                 throw new RuntimeException(response.name() +  " ürünü için stok değeri yetersiz.");
         }
-
+        boolean sent = streamBridge.send("orderCreated-out-0", order);
         return "Sipariş alındı";
     }
 }
