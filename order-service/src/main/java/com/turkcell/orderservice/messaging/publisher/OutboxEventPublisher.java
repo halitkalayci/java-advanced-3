1dae06b6fe33d545e8233ea93515cb41c0a3f8c1
halitklyc <halitenes.kalayci@gmail.com>
2025-10-14 15:00:29 +0300

    idempotence ayarları yapıldı.

--- order-service/src/main/java/com/turkcell/orderservice/messaging/publisher/OutboxEventPublisher.java
+++ order-service/src/main/java/com/turkcell/orderservice/messaging/publisher/OutboxEventPublisher.java
@@ -31,32 +31,41 @@ public class OutboxEventPublisher {
    public void publishPendingEvents() throws JsonProcessingException {
        // Her x ms'de bir bu fonksiyonu yeni bir process olarak tetikle.
        // Polling -> 5snde bir değişsede değişmesede query çalışır.
-       // CDC -> Change Data Capture -> Debezium
+       // TODO: CDC -> Change Data Capture -> Debezium
         System.out.println("Publishing pending events");
         List<OutboxMessage> pendingEvents = outboxMessageRepository.findByStatusOrderByCreatedAtAsc(OutboxStatus.PENDING);
 
         for (OutboxMessage pendingEvent : pendingEvents) {
-            OrderCreatedEvent event = objectMapper.readValue(pendingEvent.getPayloadJson(), OrderCreatedEvent.class);
+            try {
+                OrderCreatedEvent event = objectMapper.readValue(pendingEvent.getPayloadJson(), OrderCreatedEvent.class);
 
-            Message<OrderCreatedEvent> msg = MessageBuilder
-                    .withPayload(event)
-                    .setHeader("EventID", pendingEvent.getEventId())
-                    .build();
-            boolean sent = streamBridge.send("orders", msg);
+                Message<OrderCreatedEvent> msg = MessageBuilder
+                        .withPayload(event)
+                        .setHeader("EventID", pendingEvent.getEventId())
+                        .build();
+                boolean sent = streamBridge.send("orders", msg);
 
-            if(!sent)
-            {
+                if(!sent)
+                {
+                    pendingEvent.setRetryCount(pendingEvent.getRetryCount() + 1);
+                    if(pendingEvent.getRetryCount() > 5){
+                        pendingEvent.setStatus(OutboxStatus.FAILED);
+                        pendingEvent.setProcessedAt(OffsetDateTime.now());
+                    }
+                }else{
+                    pendingEvent.setStatus(OutboxStatus.SENT);
+                    pendingEvent.setProcessedAt(OffsetDateTime.now());
+                }
+
+                outboxMessageRepository.save(pendingEvent);
+
+            } catch (Exception e) {
                 pendingEvent.setRetryCount(pendingEvent.getRetryCount() + 1);
                 if(pendingEvent.getRetryCount() > 5){
                     pendingEvent.setStatus(OutboxStatus.FAILED);
                     pendingEvent.setProcessedAt(OffsetDateTime.now());
                 }
-            }else{
-                pendingEvent.setStatus(OutboxStatus.SENT);
-                pendingEvent.setProcessedAt(OffsetDateTime.now());
             }
-
-            outboxMessageRepository.save(pendingEvent);
 
         }
    }
