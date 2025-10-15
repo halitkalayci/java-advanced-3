52abd40ce2350450bd6d26e3ec60fe8e994b54e9
halitklyc <halitenes.kalayci@gmail.com>
2025-10-15 14:02:50 +0300

    add security for product-service

---
+++ product-service/src/main/java/com/turkcell/productservice/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.turkcell.productservice.config;
+
+import org.apache.http.protocol.HTTP;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                // Resource-Level Security
+                .authorizeHttpRequests(
+                        req ->        req
+                                .requestMatchers(HttpMethod.GET, "/api/v1/products/**").hasAnyAuthority("Product.Read")
+                                .requestMatchers(HttpMethod.POST, "/api/v1/products/**").hasAnyAuthority("Product.Create")
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                )
+                .build();
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(this::extractAuthorities);
+        return converter;
+    }
+
+    private Collection<GrantedAuthority> extractAuthorities(Jwt jwt)
+    {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        // ...
+        var claims = jwt.getClaims();
+
+        Map<String,Object> realmAccess = jwt.getClaim("realm_access");
+        if(realmAccess!=null)
+        {
+            List<String> roles = (List<String>) realmAccess.get("roles");
+            roles.forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
+        }
+
+        return authorities;
+    }
+}
