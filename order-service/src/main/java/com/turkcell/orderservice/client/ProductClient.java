81bd2e3610697d80de17ea86ee740ad85f3c48d6
halitklyc <halitenes.kalayci@gmail.com>
2025-10-16 15:27:37 +0300

    tracing tamamlandı.

--- order-service/src/main/java/com/turkcell/orderservice/client/ProductClient.java
+++ order-service/src/main/java/com/turkcell/orderservice/client/ProductClient.java
@@ -1,6 +1,7 @@
 package com.turkcell.orderservice.client;
 
 import com.turkcell.orderservice.contract.GetProductByIdContract;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.UUID;
 
 @FeignClient(name="product-service", path = "/api/v1/products")
+@Observed(name="product-client")
 public interface ProductClient {
     @GetMapping("{id}")
+    @Observed(name="product-client.getProductById")
     GetProductByIdContract getProductById(@PathVariable UUID id);
 }
