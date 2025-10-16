30a54b22d703c72fbb9854ed5762478609c2703b
halitklyc <halitenes.kalayci@gmail.com>
2025-10-16 10:30:20 +0300

    add product-service configurations

--- product-service/src/main/java/com/turkcell/productservice/controller/ValueController.java
+++ product-service/src/main/java/com/turkcell/productservice/controller/ValueController.java
@@ -1,12 +1,14 @@
 package com.turkcell.productservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/value")
+@RefreshScope
 public class ValueController {
     @Value("${example:NOT_FOUND}")
     private String exampleValue;
