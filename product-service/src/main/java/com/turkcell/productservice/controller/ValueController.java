8fea353e5b0b7e5660bf2a73d25a476dbbee2924
halitklyc <halitenes.kalayci@gmail.com>
2025-10-16 14:00:24 +0300

    loki ile centralized logging eklendi.

--- product-service/src/main/java/com/turkcell/productservice/controller/ValueController.java
+++ product-service/src/main/java/com/turkcell/productservice/controller/ValueController.java
@@ -1,5 +1,7 @@
 package com.turkcell.productservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +15,13 @@ public class ValueController {
     @Value("${example:NOT_FOUND}")
     private String exampleValue;
 
+    private final Logger logger = LoggerFactory.getLogger(ValueController.class);
+
+
     @GetMapping
     public String getValue() {
+        logger.info("INFO log örneği");
+        logger.warn("WARN log örneği.");
         return exampleValue;
     }
 }
